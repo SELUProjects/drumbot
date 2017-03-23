d3d055e8d7cd15f2af989bd6cb4fc4cd4a1c0ad1
billy <wewill147@gmail.com>
2017-03-22 22:38:50 -0500

    slick fresh beats

--- DrumPrototype/DrumPrototype.ino
+++ DrumPrototype/DrumPrototype.ino
@@ -10,6 +10,12 @@ int actuator3 = 8;
 int button2 = 2;
 int button7 = 7;
 int striketime = 100;
+
+int fastbeat = 0;
+int midbeat = 0;
+int slowbeat = 0;
+int beat = 0;
+
 int resttime = 1000;
 int counter = 1;
 boolean stateB1;
@@ -23,6 +29,12 @@ void setup() {
   pinMode(actuator3, OUTPUT);
   pinMode(button2, INPUT);
   pinMode(button7, INPUT);  
+  
+  // set the delay times
+  fastbeat = 150 - striketime;
+  midbeat = 300 - striketime;
+  slowbeat = 600 - striketime;
+
 }
 
 // the loop function runs over and over again forever
@@ -30,12 +42,14 @@ void loop() {
   digitalWrite(actuator1, LOW);
   digitalWrite(actuator2, LOW);
   digitalWrite(actuator3, LOW);
+  beat = midbeat;
   delay(striketime);
 
   int button2_state = digitalRead(button2);
   int button7_state = digitalRead(button7);
   
   if(button2_state == HIGH) {
+    stateB2 = false;
     if (stateB1) {
       stateB1 = false;
     } else {
@@ -45,6 +59,7 @@ void loop() {
   }
   
   if(button7_state == HIGH) {
+    stateB1 = false;
     if (stateB2) {
       stateB2 = false;
     } else {
@@ -54,53 +69,124 @@ void loop() {
   }
   
   if (stateB1) {
+      beat1();     
+  }
+  
+  if (stateB2) {    
+      beat2();
+  }
+    
+}
+
+void beat1() {
      digitalWrite(actuator1, HIGH);
      delay(striketime);
      digitalWrite(actuator1, LOW);
-     delay(500); 
+     delay(slowbeat); 
      digitalWrite(actuator3, HIGH);
      delay(striketime);
      digitalWrite(actuator3, LOW);
-     delay(500); 
-     digitalWrite(actuator1, HIGH);
-     delay(striketime);
-     digitalWrite(actuator1, LOW);
-     delay(500); 
+     delay(midbeat);  
      digitalWrite(actuator2, HIGH);
      delay(striketime);
      digitalWrite(actuator2, LOW);
-     delay(500); 
-     
-  }
-  
-  if (stateB2) {
+     delay(fastbeat); 
+     digitalWrite(actuator3, HIGH);
+     delay(striketime);
+     digitalWrite(actuator3, LOW);
+     delay(midbeat);
      digitalWrite(actuator1, HIGH);
      delay(striketime);
      digitalWrite(actuator1, LOW);
-     delay(500); 
+     delay(fastbeat); 
+}
+
+void beat2() {
+     digitalWrite(actuator1, HIGH);
+     delay(striketime);
+     digitalWrite(actuator1, LOW);
+     delay(slowbeat); 
      digitalWrite(actuator3, HIGH);
      delay(striketime);
      digitalWrite(actuator3, LOW);
-     delay(500);  
+     delay(midbeat); 
+     digitalWrite(actuator1, HIGH);
+     delay(striketime);
+     digitalWrite(actuator1, LOW);
+     delay(fastbeat); 
      digitalWrite(actuator2, HIGH);
      delay(striketime);
      digitalWrite(actuator2, LOW);
-     delay(500); 
+     delay(midbeat); 
      digitalWrite(actuator3, HIGH);
      delay(striketime);
      digitalWrite(actuator3, LOW);
-     delay(500);  
-  }
-  
-  
-  
-//  if (button2_state == HIGH) {
-//      digitalWrite(actuator1, HIGH);
-//      delay(striketime);
-//  } else if (button7_state == HIGH) {
-//      digitalWrite(actuator2, HIGH);
-//      delay(striketime);
-//  }  
+     delay(fastbeat);
+     digitalWrite(actuator3, HIGH);
+     delay(striketime);
+     digitalWrite(actuator3, LOW);
+     delay(midbeat); 
+     digitalWrite(actuator1, HIGH);
+     delay(striketime);
+     digitalWrite(actuator1, LOW);
+     delay(slowbeat); 
+}
+
+void willTell() {
+    digitalWrite(actuator1, HIGH);
+    delay(striketime);
+    digitalWrite(actuator1, LOW);
+    delay(fastbeat);
+    
+    digitalWrite(actuator2, HIGH);
+    delay(striketime);
+    digitalWrite(actuator2, LOW);
+    delay(fastbeat);
+    
+    digitalWrite(actuator1, HIGH);
+    delay(striketime);
+    digitalWrite(actuator1, LOW);
+    delay(midbeat);
+    
+    digitalWrite(actuator2, HIGH);
+    delay(striketime);
+    digitalWrite(actuator2, LOW);
+    delay(fastbeat);
+    
+    digitalWrite(actuator1, HIGH);
+    delay(striketime);
+    digitalWrite(actuator1, LOW);
+    delay(fastbeat);
+    
+    digitalWrite(actuator2, HIGH);
+    delay(striketime);
+    digitalWrite(actuator2, LOW);
+    delay(midbeat);
+    
+    digitalWrite(actuator1, HIGH);
+    delay(striketime);
+    digitalWrite(actuator1, LOW);
+    delay(fastbeat);
+    
+    digitalWrite(actuator2, HIGH);
+    delay(striketime);
+    digitalWrite(actuator2, LOW);
+    delay(fastbeat);
+    
+    digitalWrite(actuator1, HIGH);
+    delay(striketime);
+    digitalWrite(actuator1, LOW);
+    delay(midbeat);
+    
+    digitalWrite(actuator2, HIGH);
+    delay(striketime);
+    digitalWrite(actuator2, LOW);
+    delay(midbeat);
+    
+    digitalWrite(actuator1, HIGH);
+    delay(striketime);
+    digitalWrite(actuator1, LOW);
+    delay(slowbeat);
   
 }
 
