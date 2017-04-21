2081d87a75b99b54c098f8ae5f0e1ae07ae1ac16
billy <wewill147@gmail.com>
2017-04-21 17:06:53 -0500

    added the rpi to control the arduino

---
+++ nanpyapi/arduino/arduino.py
@@ -0,0 +1,73 @@
+from flask import Flask, Blueprint, Response, request, json
+from nanpy import (ArduinoApi, SerialManager)
+from time import sleep
+
+arduino = Blueprint('arduino', __name__)
+
+leftDS = 7
+rightDS = 6
+tamborine = 5
+
+connection = SerialManager()
+a = ArduinoApi(connection=connection)
+a.pinMode(leftDS, a.OUTPUT)
+a.pinMode(rightDS, a.OUTPUT)
+a.pinMode(tamborine, a.OUTPUT)
+
+
+@arduino.route('/')
+def drumbot():
+    return 'Drumbot says "Beat it!"'
+
+
+@arduino.route('/play/<int:beat>')
+def play(beat):
+    striketime = .10
+    fastbeat = .15 - striketime
+    midbeat = .30 - striketime
+    slowbeat = .60 - striketime
+    data = {'beat played': beat}
+
+    if beat == 1:
+        for i in range(3):
+            a.digitalWrite(leftDS, a.HIGH)
+            sleep(striketime)
+            a.digitalWrite(leftDS, a.LOW)
+            sleep(slowbeat)
+            a.digitalWrite(rightDS, a.HIGH)
+            sleep(striketime)
+            a.digitalWrite(rightDS, a.LOW)
+            sleep(slowbeat)
+            a.digitalWrite(tamborine, a.HIGH)
+            sleep(striketime)
+            a.digitalWrite(tamborine, a.LOW)
+            sleep(slowbeat)
+
+        return Response(json.dumps(data), status=200, mimetype='application/json')
+    
+    if beat == 2:
+        for i in range(3):
+            a.digitalWrite(rightDS, a.HIGH)
+            sleep(striketime)
+            a.digitalWrite(rightDS, a.LOW)
+            sleep(slowbeat)
+            a.digitalWrite(tamborine, a.HIGH)
+            sleep(striketime)
+            a.digitalWrite(tamborine, a.LOW)
+            sleep(slowbeat)
+            a.digitalWrite(leftDS, a.HIGH)
+            sleep(striketime)
+            a.digitalWrite(leftDS, a.LOW)
+            sleep(slowbeat)
+            a.digitalWrite(tamborine, a.HIGH)
+            sleep(striketime)
+            a.digitalWrite(tamborine, a.LOW)
+            sleep(slowbeat)
+            a.digitalWrite(tamborine, a.HIGH)
+            sleep(striketime)
+            a.digitalWrite(tamborine, a.LOW)
+            sleep(slowbeat)
+
+        return Response(json.dumps(data), status=200, mimetype='application/json')
+
+    return 'You have no beat.'
---
+++ nanpyapi/nanpyapi.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from arduino.arduino import arduino
+
+app = Flask(__name__)
+app.register_blueprint(arduino, url_prefix='/arduino')
+
+if __name__ == '__main__':
+    app.config['TRAP_BAD_REQUEST_ERRORS'] = True
+    app.debug = True
+    app.run(host='0.0.0.0', port=80)
+
